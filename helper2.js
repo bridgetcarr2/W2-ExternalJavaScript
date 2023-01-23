47c921ebc80c7639b12452bede8029506fa4d97f
bridgetcarr2 <122331284+bridgetcarr2@users.noreply.github.com>
2023-01-23 00:13:13 -0500

    Add files via upload

---
+++ helper2.js
@@ -0,0 +1,10 @@
+
+function displayText(){
+document.getElementById("Bridget").innerHTML="About me! My hobbies include gardening, yoga, hiking and painting.";
+}
+
+document.getElementById("name").innerText = "Bridget Carr";
+document.getElementById("major").innerText = "Digital Marketing & Advertising Art";
+document.getElementById("email").innerText = "Bricarr@uat.edu";
+document.getElementById("graduation").innerText = "August 2023";
+
