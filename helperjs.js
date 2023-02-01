778799a522b342258b1a230b4e369de2f10d2a36
bridgetcarr2 <122331284+bridgetcarr2@users.noreply.github.com>
2023-01-31 19:15:04 -0500

    Add files via upload
    Added comments, and updated screenshot

---
+++ helperjs.js
@@ -0,0 +1,15 @@
+
+//this function takes  an html element id and text
+function displayText(){
+     document.getElementById("myText").innerHTML ="My Job: I work in a floral department, at Wegmans.";
+}
+
+//this function takes  an html element id and text
+function displayText2(htmlElement){
+    document.getElementById(htmlElement).innerHTML ="My Hobbies: Yoga, gardening, painting, cooking, and taking care of my pets.";
+}
+
+//this function takes  an html element id and text
+function displayText3(htmlElement){
+    document.getElementById(htmlElement).innerHTML ="Quote: Happiness held is the seed; Happiness shared in the flower- John Harrigan";
+}
